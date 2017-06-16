4377d442e4e19446e6409411fbd1c73ddcfdacf1
Dan <d.brady@bbk.ac.uk>
2017-06-16 13:54:41 +0800

    Updated to output to CSV file

--- Overflow_Behav_data_process.m
+++ Overflow_Behav_data_process.m
@@ -2,9 +2,15 @@
 dataDir = '/Volumes/Samsung/EEG Data/DCD/Hana/Behavioural';
 cd(dataDir)
 
+outputFName  = 'MeanFreq.csv';
+outputHeader = 'ID, Group, Slow, Medium, Fast\n';
+
 files = dir('P*.mat');
 
 testOutput = nan(length(files),3);
+
+fid = fopen(outputFName, 'w');                     % Create and open output data file
+fprintf(fid, outputHeader);
 
 for file_n = 1:length(files)
     
@@ -18,4 +24,10 @@ for file_n = 1:length(files)
     
     testOutput(file_n,:) = mean(cellfun('length', OverflowBehavData.(fileID))/5);
     
+    a = strsplit(fileID, '_');
+    
+    fprintf(fid,'P%0d,%s,%.3f,%.3f,%.3f\n',file_n, a{2},testOutput(1,:));
+    
 end
+
+fclose(fid);
