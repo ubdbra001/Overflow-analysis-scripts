d67ab675169886fc5aa8018b009a34178e8a0ac2
Dan <d.brady@bbk.ac.uk>
2017-06-16 14:12:07 +0800

    Commented up new scripts

--- BehavStatAnalysis.R
+++ BehavStatAnalysis.R
@@ -1,13 +1,22 @@
+# Load dependencies
 library(reshape2)
 library(readr)
 library(ez)
 
+# Import data as dataframe
 MeanFreq = as.data.frame(read_csv("/Volumes/Samsung/EEG Data/DCD/Hana/Behavioural/MeanFreq.csv"))
 
+# Change to format suitable for ANOVA
 longFreq = melt(MeanFreq)
+
+# Give names to each variable
 names(longFreq) = c("ID", "Group", "Speed", "Value")
 
+# Turn Speed variable into a factor
 longFreq$Speed = factor(longFreq$Speed, labels = c("Slow", "Medium", "Fast"))
+
+# Reorder by Participant ID
 longFreq = longFreq[order(longFreq$ID),]
 
+# Grnerate ANOVA model
 freqModel<-ezANOVA(data = longFreq, dv = .(Value), wid = .(ID), within = .(Speed), between = .(Group), detailed = TRUE, type = 3)
--- Overflow_Behav_data_process.m
+++ Overflow_Behav_data_process.m
@@ -5,28 +5,28 @@ cd(dataDir)
 outputFName  = 'MeanFreq.csv';
 outputHeader = 'ID, Group, Slow, Medium, Fast\n';
 
-files = dir('P*.mat');
+files = dir('P*.mat');              % Find all data files
 
-testOutput = nan(length(files),3);
+testOutput = nan(length(files),3);  % Preallocate output variable
 
-fid = fopen(outputFName, 'w');                     % Create and open output data file
-fprintf(fid, outputHeader);
+fid = fopen(outputFName, 'w');      % Create and open output data file
+fprintf(fid, outputHeader);         % Write header to file
 
-for file_n = 1:length(files)
+for file_n = 1:length(files)        % Loop through each file
     
-    fileName = files(file_n).name;
-    [~, fileID, ~] = fileparts(fileName);
+    fileName = files(file_n).name;  % Generate filename
+    [~, fileID, ~] = fileparts(fileName); % Generate ID
     
-    load(fileName)
+    load(fileName)                  % Load file
     
-    OverflowBehavData.(fileID) = results;
-    clear results
+    OverflowBehavData.(fileID) = results; % Place in grouped fine (currently this is not strictly needed)
+    clear results                   % Clear loaded file
     
-    testOutput(file_n,:) = mean(cellfun('length', OverflowBehavData.(fileID))/5);
+    testOutput(file_n,:) = mean(cellfun('length', OverflowBehavData.(fileID))/5); % Calculate mean tapping frequency
     
-    a = strsplit(fileID, '_');
+    condition = strsplit(fileID, '_');  % Get condition
     
-    fprintf(fid,'P%0d,%s,%.3f,%.3f,%.3f\n',file_n, a{2},testOutput(1,:));
+    fprintf(fid,'P%0d,%s,%.3f,%.3f,%.3f\n',file_n, condition{2},testOutput(1,:)); % Write output to CSV
     
 end
 
